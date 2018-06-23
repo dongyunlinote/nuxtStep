716cb2b991b8b1aa9695b4438ed82a77b568d68d
董云丽 <dongyunli@315free.com>
2018-06-23 14:45:12 +0800

    add

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -9,8 +9,11 @@
         <li><nuxt-link :to="{name:'news-id',params:{id:123}}">News-1 全局路由动画效果</nuxt-link></li>
 
 
-
         <li><nuxt-link :to="{name:'news-id',params:{id:123,title:'jspang.com'}}">News-1 个性meta设置</nuxt-link></li>
+        
+        <nuxt-link :to="{name:'newsChild'}">跳转到子路由</nuxt-link>
+        <a href="/news/newsChild">跳转到子路由</a>
+
       </ul>
   </div>
 </template>
